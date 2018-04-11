601a4e948c9ff91a4f296ee194905f5bd3e03c82
Andre van Tonder <drevtonder@gmail.com>
2018-04-11 21:02:51 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import discord
+import asyncio
+import random
+import re
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+client = discord.Client()
+
+bot = ChatBot(
+    'Fred',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    database='./db.sqlite3'
+)
+
+bot.set_trainer(ChatterBotCorpusTrainer)
+
+'''bot.train(
+    "chatterbot.corpus.english",
+)'''
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    clean_message = remove_mentions(message)
+    if client.user in message.mentions or random.randint(0,100) < 10 or message.channel.is_private:
+        print("message: " + clean_message)
+        bot_reply = str(bot.get_response(clean_message))
+        await client.send_message(message.channel, content=bot_reply)
+        print("reply: " + bot_reply)
+    else:
+        msg = await client.wait_for_message(channel=message.channel)
+        bot.learn_response(remove_mentions(msg),remove_mentions(msg))
+
+def remove_mentions(message):
+    return re.sub(r'@[a-zA-Z\d\S:]+','',message.clean_content).strip()
+
+client.run('NDMzNTg5MTE2MDA2MDM5NTYz.Da-C4g.6ByagOSok2vnhlrNRDgB7gO4ang')
