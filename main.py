338fdb254c58e118ee08b455f1abd5d88c47f927
Andre van Tonder <drevtonder@gmail.com>
2018-04-21 17:39:22 +0800

    basic commands

--- main.py
+++ main.py
@@ -19,29 +19,41 @@ from chatterbot.conversation import Statement
 def remove_mentions(message):
     return re.sub(r'@[a-zA-Z\d\S:]+','',message.clean_content).strip()
 
-class MyClient(discord.Client):
-    def __init__(self, *args):
-        super(MyClient, self).__init__(*args)
+def get_command(message, prefix="!"):
+    if not message.content.startswith(prefix):
+        raise Exception("No command")
+    return message.content.split(" ")[0].replace("!", " ").strip()
+    
+
+class ChatClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super(ChatClient, self).__init__(*args, **kwargs)
 
         self.bot = ChatBot(
             'Fred',
             storage_adapter='chatterbot.storage.SQLStorageAdapter',
             database='./db.sqlite3'
-        ) 
+        )
+
+        self.commands = {
+            'ping': self.ping,
+            'train': self.train
+        }
 
     @asyncio.coroutine
-    def on_ready(self):
-        logging.info('Connected!')
-        logging.info('Username: ' + self.user.name)
-        logging.info('ID: ' + self.user.id)
+    def ping(self, message):
+        yield from self.send_message(message.channel, content="pong")
 
+    @asyncio.coroutine
+    def train(self, message):
+        yield from self.send_message(message.channel, content="train")
+    
     def corpus_train():
         self.bot.set_trainer(ChatterBotCorpusTrainer)
 
         self.bot.train(
             "chatterbot.corpus.english",
         )
-
 
     def learn(self, message, reply):
         logging.info("--- Learning ---")
@@ -58,6 +70,13 @@ class MyClient(discord.Client):
         logging.info("learning message: " + message)
         logging.info("learning reply: " + reply)
 
+    @asyncio.coroutine
+    def on_ready(self):
+        logging.info('Connected!')
+        logging.info('Username: ' + self.user.name)
+        logging.info('ID: ' + self.user.id)          
+
+
     @asyncio.coroutine  
     def send_reply(self, message):
         logging.info("--- Sending Reply ---")
@@ -67,12 +86,20 @@ class MyClient(discord.Client):
         yield from self.send_message(message.channel, content=reply)
 
         logging.info("message: " + message_without_mentions)
-        logging.info("reply: " + reply)
-        
+        logging.info("reply: " + reply)     
 
     @asyncio.coroutine  
     def on_message(self,message):
-        if message.author == self.user or "!" in message.content:
+        if message.author == self.user:
+            return
+
+        if message.content.startswith("!"):
+            command = get_command(message)
+            try:
+                yield from self.commands[command](message)
+            except KeyError:
+                yield from self.send_message(message.channel, content=(command + " is not a registered command :P"))
+
             return
         
         if self.user in message.mentions or message.channel.is_private:
@@ -86,21 +113,22 @@ class MyClient(discord.Client):
                 
                 self.learn(message, reply)
 
-if("-train" in sys.argv):
-    corpus_train()
+def main():
+    if("-train" in sys.argv):
+        corpus_train()
 
-if '-token' not in sys.argv and 'token' not in os.environ:
-    raise Exception("Please supply a token using '-token YOUR-TOKEN-HERE' or setting the environment variable token")
-else:
-    token = ""
-    if '-token' in sys.argv: 
-        token = sys.argv[sys.argv.index('-token') + 1]
+    if '-token' not in sys.argv and 'token' not in os.environ:
+        raise Exception("Please supply a token using '-token YOUR-TOKEN-HERE' or setting the environment variable 'token'")
     else:
-        token = os.environ['token']
+        token = ""
+        if '-token' in sys.argv: 
+            token = sys.argv[sys.argv.index('-token') + 1]
+        else:
+            token = os.environ['token']
 
-    client = MyClient()
-    client.run(token)
+        client = ChatClient()
+        client.run(token)
+
+if __name__ == '__main__':
+    main()
     
-
-    
-
